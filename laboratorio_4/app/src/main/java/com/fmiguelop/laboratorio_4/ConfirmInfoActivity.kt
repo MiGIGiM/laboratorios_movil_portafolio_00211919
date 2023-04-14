e7efc52e358e169b059dfb73a256c6128b1ba2a3
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-04-14 15:11:05 -0600

    feat(add): labo 4

---
+++ laboratorio_4/app/src/main/java/com/fmiguelop/laboratorio_4/ConfirmInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.fmiguelop.laboratorio_4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_info)
+    }
+}
